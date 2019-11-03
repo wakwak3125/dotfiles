0bec4bbbbc01a92a102db990fea4bd2c8c8389e2
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2019-11-03 19:05:01 +0900

    Add jb ide settings

--- config.py
+++ config.py
@@ -6,4 +6,14 @@ define_modmap({
     Key.LEFT_CTRL: Key.CAPSLOCK,
 })
 
+define_conditional_modmap(lambda wm_class: wm_class in ("FocusProxy"), {
+    Key.CAPSLOCK: Key.LEFT_CTRL,
+    Key.LEFT_CTRL: Key.CAPSLOCK,
+    Key.LEFT_ALT: Key.LEFT_META,
+    Key.LEFT_META: Key.LEFT_ALT,
+})
+
+define_keymap(lambda wm_class: wm_class in ("FocusProxy"), {
+    K("Super-q"): K("M-q"),
+})
 
