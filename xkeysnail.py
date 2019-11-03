4a40aa02907969fa88e6a989eca300d52b230730
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2019-11-03 18:59:22 +0900

    Tweak

--- xkeysnail.py
+++
@@ -1,9 +0,0 @@
-import re
-from xkeysnail.transform import *
-
-define_modmap({
-    Key.CAPSLOCK: Key.LEFT_CTRL,
-    Key.LEFT_CTRL: Key.CAPSLOCK,
-})
-
-
--- xkeysnail.sh
+++ xkeysnail.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# --device /dev/input/event8 'SteelSeries SteelSeries Apex M750 TKL'
 if [ -x /usr/local/bin/xkeysnail ]; then
     xhost +SI:localuser:xkeysnail
-    sudo -u xkeysnail DISPLAY=$DISPLAY /usr/local/bin/xkeysnail $HOME/dotfiles/xkeysnail.py
+    sudo -u xkeysnail DISPLAY=$DISPLAY /usr/local/bin/xkeysnail $HOME/dotfiles/config.py
 fi
 
