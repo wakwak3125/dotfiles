5d1601328d8f325ea7d1cccfbf6b0f20bf63f7c1
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2019-12-19 23:31:28 +0900

    Tweak bootstrap script to install fzf

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,19 +1,26 @@
 if [ -e $HOME/.vimrc ]; then
-	echo '.vimrc is already exists'
+	echo '.vimrc found'
 else
 	ln -s _vimrc $HOME/.vimrc
 fi
 
 if [ -e $HOME/.gvimrc ]; then
-	echo '.gvimrc is already exists'
+	echo '.gvimrc found'
 else
 	ln -s _gvimrc $HOME/.gvimrc
 fi
 
 if [ -e $HOME/.ideavimrc ]; then
-	echo '.ideavimrc is already exists'
+	echo '.ideavimrc found'
 else
 	ln -s _ideavimrc $HOME/.ideavimrc
+fi
+
+if [ -e $HOME/.fzf ]; then
+	echo '.fzf found'
+else
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	~/.fzf/install
 fi
 
 mkdir ./_vim/plugged
