1a15d4927eecd96ed5a62da58d01fccf6bcf072d
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2020-04-03 11:03:44 +0900

    Add fzf git

---
+++ zsh/fzf-ghq.zsh
@@ -0,0 +1,11 @@
+function ghq-fzf() {
+  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  if [ -n "$src" ]; then
+    BUFFER="cd $(ghq root)/$src"
+    zle accept-line
+  fi
+  zle -R -c
+}
+zle -N ghq-fzf
+bindkey '^]' ghq-fzf
+
