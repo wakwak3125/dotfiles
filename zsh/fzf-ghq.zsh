219b1907ee2725d36a147959df9733dba38fbb5d
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2020-04-03 11:07:41 +0900

    Tweak

--- zsh/fzf-ghq.zsh
+++ zsh/fzf-ghq.zsh
@@ -1,5 +1,5 @@
 function ghq-fzf() {
-  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  local src=$(ghq list | fzf --preview "ls -laT $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
   if [ -n "$src" ]; then
     BUFFER="cd $(ghq root)/$src"
     zle accept-line
