e465d0cedc844e2adb24319a83e65e06c64e34a5
Ryo Sakaguchi <rsakaguchi3125@gmail.com>
2020-04-21 08:12:52 +0900

    Tweak script

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 cd `dirname $0`
 ROOT=`dirname $(pwd)`
@@ -19,3 +19,11 @@ else
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+if !(type "brew" > /dev/null 2>&1); then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+  echo "eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> $HOME/.zshrc
+fi
+
+brew update
+brew install ghq anyenv zplug
+anyenv install --force-init
